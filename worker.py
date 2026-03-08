f5a307b07545f78877830f5d9c5382210ddabc23
cheyenne furlong <113577505+cheyenneIT22@users.noreply.github.com>
2026-03-09 00:35:33 +1000

    Implement main entry point for agent through a worker file

---
+++ worker.py
@@ -0,0 +1,9 @@
+from livekit.agents import cli, WorkerOptions
+from agent import entrypoint
+
+if __name__ == "__main__":
+    cli.run_app(
+        WorkerOptions(
+            entrypoint_fnc=entrypoint
+        )
+    )
